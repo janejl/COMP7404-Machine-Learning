069dd8589b3ac8836533c71ae38303f5d42439b5
janejl <jane.hkucs@gmail.com>
2019-10-21 16:03:34 +0800

    a2 q3 done

--- a2/analysis.py
+++ a2/analysis.py
@@ -11,37 +11,42 @@ def question2():
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Prefer the close exit (+1), risking the cliff (-10)
+    answerDiscount = 0.1
+    answerNoise = 0
+    answerLivingReward = 0
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Prefer the close exit (+1), but avoiding the cliff (-10)
+    answerDiscount = 0.3
+    answerNoise = 0.3
+    answerLivingReward = 0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Prefer the distant exit (+10), risking the cliff (-10)
+    answerDiscount = 0.9
+    answerNoise = 0
+    answerLivingReward = 0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Prefer the distant exit (+10), avoiding the cliff (-10)
+    answerDiscount = 0.9
+    answerNoise = 0.2
+    answerLivingReward = 0.1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # Avoid both exits and the cliff (so an episode should never terminate)
+    answerDiscount = 0
+    answerNoise = 0
+    answerLivingReward = 100
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
